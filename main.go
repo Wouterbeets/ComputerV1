9ce952179eaa179e9a2718e877b3388c3a115e35
Wouter Beets <beetswouter@gmail.com>
2015-06-05 11:38:48 +0200

    boo

--- main.go
+++ main.go
@@ -1,62 +1,20 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
+	"github.com/Wouterbeets/poly"
 	"os"
-	"strconv"
-	"strings"
 )
-
-type poly struct {
-	degree   int
-	eqString string
-	terms    []term
-	operator []string
-}
-
-type term struct {
-	multip int
-	indet  string
-	power  int
-}
-
-func (t *term) parseMul(mulStr string) error {
-	mul, err := strconv.Atoi(mulStr)
-	if err != nil {
-		return err
-	}
-	t.multip = mul
-	return nil
-}
-
-func (t *term) checkMulOp(opStr string) error {
-	if opStr == "*" {
-		return nil
-	} else {
-		return errors.New("operator before indeterminate must be \"*\"")
-	}
-}
-
-func (t *term) parsePow(powStr string) error {
-	indPow := strings.Split(powStr, "^")
-	t.indet = indPow[0]
-	p, err := strconv.Atoi(indPow[1])
-	if err != nil {
-		return err
-	}
-	t.power = p
-	return nil
-}
 
 func main() {
 	flag.Parse()
 	if flag.NArg() != 1 {
 		usage(nil)
 	} else {
-		eq := &poly{}
-		eq.parseEq(flag.Arg(0))
+		eq := &poly.Poly{}
+		err := eq.ParseEq(flag.Arg(0))
+		errorHandel(err)
 		fmt.Printf("%+v\n", eq)
 	}
 }
@@ -73,26 +31,4 @@ func usage(err error) {
 	}
 	fmt.Println("usage example: ./ComputerV1 \"2 * X^2 + 3 * X^1 * 14 * X^2\"")
 	os.Exit(-1)
-}
-
-func (eq *poly) parseEq(str string) {
-	termStrs := strings.Split(str, " ")
-	fmt.Printf("%+v\n", str)
-	for i := 0; i < len(termStrs); i++ {
-		t := term{}
-		err := t.parseMul(termStrs[i])
-		errorHandel(err)
-		err = t.checkMulOp(termStrs[i+1])
-		errorHandel(err)
-		err = t.parsePow(termStrs[i+2])
-		errorHandel(err)
-		if err != nil {
-			panic(err)
-		}
-		i = i + 3
-		if ((i+1)%4) == 0 && i != 0 && i < len(termStrs) {
-			eq.operator = append(eq.operator, termStrs[i])
-		}
-		eq.terms = append(eq.terms, t)
-	}
 }
